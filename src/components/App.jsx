8e736cfdd2c6fb2f97df9536049b8b4ba5477b48
IvanPanin675 <jovanni.94.van@gmail.com>
2023-02-10 20:04:09 +0200

    hm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Movies } from "pages/Movies";
-import { Container, Header, Logo, Link } from "./App.styled";
-
+import { Routes, Route, Link } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Movies } from 'pages/Movies';
+import { NotFound } from 'pages/NotFound';
 
 export const App = () => {
   return (
     <div>
-      <nav><Link to="/movies">Movies</Link></nav>
-      
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        {/* <Route path="/products" element={<Products />} />
-        <Route path="*" element={<NotFound />} /> */}
+        {/* <Route path="/products" element={<Products />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
